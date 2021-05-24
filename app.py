8b5cab5bb4659aff2048d4858b85eacdce3a731f
Chris Chiang <chris23chiang@gmail.com>
2021-05-24 18:36:20 +0800

    update

--- app.py
+++ app.py
@@ -43,11 +43,14 @@ def handle_message(event):
     msg = event.message.text
     s = '很抱歉，您說什麼?'
 
-    if msg == 'hi':
-        s = 'hi!'
-
+    if msg in ['hi', 'Hi']:
+        s = '嗨!'
     elif msg == '你吃飯了嗎':
         s = '還沒'
+    elif msg == '你是誰':
+        s = '我是機器人'
+    elif '訂位' in msg:
+        s = '你想訂位，是嗎?'
 
     line_bot_api.reply_message(
         event.reply_token,
