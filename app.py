c9580046e108f234b09815d100cbb6d4261bfdad
Chris Chiang <chris23chiang@gmail.com>
2021-05-24 18:21:59 +0800

    update

--- app.py
+++ app.py
@@ -40,9 +40,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    s = '中信兄弟總冠軍'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=s))
 
 
 if __name__ == "__main__":
